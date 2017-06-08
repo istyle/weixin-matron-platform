080adb42ae3ce1d8eff28702e878e435b5c08a56
zhangjie <i@izhangjie.com>
2017-06-08 10:46:38 +0800

    微信平台

--- wx-platfrom-web/src/main/java/com/geekunite/wx/web/WebStartApplication.java
+++ wx-platfrom-web/src/main/java/com/geekunite/wx/web/WebStartApplication.java
@@ -2,19 +2,13 @@ package com.geekunite.wx.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@Controller
+@Configuration
+@ComponentScan
 @EnableAutoConfiguration
 public class WebStartApplication {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WebStartApplication.class, args);
