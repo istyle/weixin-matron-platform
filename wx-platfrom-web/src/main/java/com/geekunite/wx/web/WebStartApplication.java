392ef330f186de9462b3c3e428dd0d4b5e11eb82
zhangjie <i@izhangjie.com>
2017-06-14 01:03:07 +0800

    修改代码

--- wx-platfrom-web/src/main/java/com/geekunite/wx/web/WebStartApplication.java
+++ wx-platfrom-web/src/main/java/com/geekunite/wx/web/WebStartApplication.java
@@ -1,16 +1,21 @@
 package com.geekunite.wx.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
-@EnableAutoConfiguration
-public class WebStartApplication {
+@SpringBootApplication
+public class WebStartApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(WebStartApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebStartApplication.class);
 	}
 }
--- wx-platfrom-web/src/main/resources/templates/error.ftl
+++ wx-platfrom-web/src/main/resources/templates/error.ftl
@@ -2,8 +2,6 @@
 <body>
 	<h2>Hello World!111</h2>
 	123123
-	#if(true)
-		123123
-	#end
+	<#if 1=2>123123</#if>
 </body>
 </html>
