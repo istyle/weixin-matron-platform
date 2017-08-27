4abf115ea7efa41630d837a259592a479a38cd80
zhangjie <i@izhangjie.com>
2017-08-27 18:56:10 +0800

    添加dubbo接口

--- wx-platfrom-web/src/main/java/com/geekunite/wx/web/handler/SubscribeHandler.java
+++ wx-platfrom-web/src/main/java/com/geekunite/wx/web/handler/SubscribeHandler.java
@@ -1,10 +1,15 @@
 package com.geekunite.wx.web.handler;
 
+import java.util.Date;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.geekunite.wx.web.builder.TextBuilder;
+import com.iwamama.member.facade.IWxUserServiceFacade;
+import com.iwamama.member.pojo.WxUserPOJO;
+import com.iwamama.platform.common.ApiResult;
 
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
@@ -15,32 +20,34 @@ import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 @Component
 public class SubscribeHandler extends AbstractHandler {
 
+	@Autowired
+	private IWxUserServiceFacade wxUserServiceFacade;
+
 	@Override
 	public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService weixinService, WxSessionManager sessionManager) throws WxErrorException {
 
-		this.logger.info("新关注用户 OPENID: " + wxMessage.getFromUser());
+		String openid = wxMessage.getFromUser();
+		this.logger.info("新关注用户 OPENID: {}", openid);
 
 		// 获取微信用户基本信息
 		// WxMpUser userWxInfo =
 		// weixinService.getUserService().userInfo(wxMessage.getFromUser(),
 		// null);
-
-		if (wxMessage.getFromUser() != null) {
-
-		}
-
-		WxMpXmlOutMessage responseResult = null;
 		try {
-			responseResult = handleSpecial(wxMessage);
-		} catch (Exception e) {
-			this.logger.error(e.getMessage(), e);
-		}
+			ApiResult<WxUserPOJO> apiResult = wxUserServiceFacade.getWxUserByOpenid(openid);
+			if (apiResult.getResult() == null) {
+				WxUserPOJO wxUserPOJO = new WxUserPOJO();
+				Date now = new Date();
+				wxUserPOJO.setOpenid(openid);
+				wxUserPOJO.setCreateDate(now);
+				wxUserPOJO.setUpdateDate(now);
+				wxUserServiceFacade.insert(wxUserPOJO);
+			}
+			WxMpXmlOutMessage responseResult = handleSpecial(wxMessage);
+			if (responseResult != null) {
+				return responseResult;
+			}
 
-		if (responseResult != null) {
-			return responseResult;
-		}
-
-		try {
 			return new TextBuilder().build("感谢关注", wxMessage, weixinService);
 		} catch (Exception e) {
 			this.logger.error(e.getMessage(), e);
